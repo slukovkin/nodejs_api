646285e7cb2b36bd843427e0ccc1f41a54abc4bc
Sergey Lukovkin <slukovkin@icloud.com>
2023-02-25 23:26:14 +0200

    update comment

--- router/router.js
+++ router/router.js
@@ -12,7 +12,7 @@ const router = new Router()
 router.get("/user/:id", findUserOne) // Поиск пользоватля в БД по ID
 router.get("/user", findUserAll) // Поиск всех пользователей в БД
 router.post("/user", createUser) // Создание пользователя в БД
-router.put("/user/:id", updateUser) // Изменение данных пользователя в БД по Email
+router.put("/user/:id", updateUser) // Изменение данных пользователя в БД по ID
 router.delete("/user", deleteUser) // Удаление пользователя из БД по ID
 
 export default router
