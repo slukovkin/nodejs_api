46a0f2d0a7708092dc79976ee53538a8a9b23eae
Sergey Lukovkin <slukovkin@icloud.com>
2023-02-25 23:20:35 +0200

    added full CRUD operation

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -31,15 +31,80 @@ export const createUser = async (req, res) => {
 export const findUserOne = async (req, res) => {
   try {
     const id = req.params.id
-    const user = await User.findOne({where: {id: id}})
-    if(!user) {
+    const user = await User.findOne({ where: { id: id } })
+    if (!user) {
       return res.status(200).json({
-        message: 'Пользователь не найден'
+        message: "Пользователь не найден",
       })
     }
     res.status(200).json({
-      message: 'Пользователь найден в БД',
-      user
+      message: "Пользователь найден в БД",
+      user,
+    })
+  } catch (error) {
+    res.status(400).json({
+      message: "Что-то пошло не так",
+      error,
+    })
+  }
+}
+
+export const findUserAll = async (req, res) => {
+  try {
+    const users = await User.findAll()
+    if (!users) {
+      return res.status(200).json({
+        message: "Пользователей в БД не найдено",
+      })
+    }
+    res.status(200).json({
+      message: `Количество пользователей в БД: ${users.length}`,
+      users,
+    })
+  } catch (error) {
+    res.status(400).json({
+      message: "Что-то пошло не так",
+      error,
+    })
+  }
+}
+
+export const updateUser = async (req, res) => {
+  try {
+    const { email, password, avatarUrl } = req.body
+    await User.update(
+      {
+        login: email.match(/^([\w ]+)/gms).toString(),
+        email: email,
+        password: await hashPassword(password),
+        avatarUrl: avatarUrl
+      },
+      {
+        where: { id: req.params.id },
+      }
+    )
+    res.status(201).json({
+      message: 'Данные пользователя успешно обновлены в БД'
+    })
+  } catch (error) {
+    res.status(400).json({
+      message: "Что-то пошло не так",
+      error,
+    })
+  }
+}
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { email } = req.body
+    const user = await User.destroy({ where: { email } })
+    if (!user) {
+      return res.status(200).json({
+        message: "Пользователь не найден в БД",
+      })
+    }
+    res.status(200).json({
+      message: `Пользователь успешно удален из БД`,
     })
   } catch (error) {
     res.status(400).json({
--- router/router.js
+++ router/router.js
@@ -1,9 +1,18 @@
 import Router from "express"
-import { createUser, findUserOne } from "../controllers/user_controller.js"
+import {
+  createUser,
+  findUserOne,
+  findUserAll,
+  deleteUser,
+  updateUser,
+} from "../controllers/user_controller.js"
 
 const router = new Router()
 
-router.get('/user/:id', findUserOne) // Поиск пользоватля в БД по ID
+router.get("/user/:id", findUserOne) // Поиск пользоватля в БД по ID
+router.get("/user", findUserAll) // Поиск всех пользователей в БД
 router.post("/user", createUser) // Создание пользователя в БД
+router.put("/user/:id", updateUser) // Изменение данных пользователя в БД по Email
+router.delete("/user", deleteUser) // Удаление пользователя из БД по ID
 
 export default router
