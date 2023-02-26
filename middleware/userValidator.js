9fa794daf9b356178f0c64603af3aff1aa55a9b4
Sergey Lukovkin <slukovkin@icloud.com>
2023-02-26 12:59:20 +0200

    add user validator

---
+++ middleware/userValidator.js
@@ -0,0 +1,7 @@
+import { body } from "express-validator"
+
+export const checkedUserData = [
+  body('email', "Email должен быть корректный").isEmail(),
+  body('password', "Пароль не менее 5 символов").isLength({ min: 5 }),
+  body('avatarUrl', "Аватар должен быть ссылкой на изображение").isString(),
+]
