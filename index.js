a609258ff12f81d0ba916128f8b26140d834b299
Sergey Lukovkin <slukovkin@icloud.com>
2023-02-26 13:28:13 +0200

    refactoring router

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express()
 app.use(express.json())
 
 app.use('/api', router)
+app.use('/auth', router)
 
 db_init()
 
--- router/router.js
+++ router/router.js
@@ -1,19 +1,17 @@
 import Router from "express"
-import {
-  createUser,
-  findUserOne,
-  findUserAll,
-  deleteUser,
-  updateUser,
-} from "../controllers/user_controller.js"
+import * as userController from "../controllers/user_controller.js"
 import { checkedUserData } from "../middleware/userValidator.js"
 
 const router = new Router()
 
-router.get("/user/:id", findUserOne) // Поиск пользоватля в БД по ID
-router.get("/user", findUserAll) // Поиск всех пользователей в БД
-router.post("/user", checkedUserData, createUser) // Создание пользователя в БД
-router.put("/user/:id", updateUser) // Изменение данных пользователя в БД по ID
-router.delete("/user", deleteUser) // Удаление пользователя из БД по ID
+router.get("/", (req, res) => {
+  res.send("Аудентификация пользователя")
+})
+
+router.get("/user/:id", userController.findUserOne) // Поиск пользоватля в БД по ID
+router.get("/user", userController.findUserAll) // Поиск всех пользователей в БД
+router.post("/user", checkedUserData, userController.createUser) // Создание пользователя в БД
+router.put("/user/:id", userController.updateUser) // Изменение данных пользователя в БД по ID
+router.delete("/user", userController.deleteUser) // Удаление пользователя из БД по ID
 
 export default router
