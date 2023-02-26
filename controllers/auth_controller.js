33cd84c90a2ebc5b0d986447d6cd875f81631147
Sergey Lukovkin <slukovkin@icloud.com>
2023-02-26 14:43:25 +0200

    add jwt token

---
+++ controllers/auth_controller.js
@@ -0,0 +1,42 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { env } from "../config/config.js"
+import { User } from "../database/models/User.js"
+
+export const auth = (req, res) => {}
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ where: { email: email } })
+    if (!user) {
+      return res.status(400).json({
+        message: "Ошибка при запросе пользователя",
+      })
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        message: "Ошибка аудентификации. Доступ запрещен",
+      })
+    }
+    const token = jwt.sign(
+      {
+        data: user.email,
+      },
+      env.SECRET,
+      { expiresIn: "1h" }
+    )
+    res.status(200).json({
+      message: "Доступ разрешен",
+      user,
+      token,
+    })
+  } catch (error) {
+    res.status(400).json({
+      message: "Ошибка запроса",
+    })
+  }
+}
+
+export const logout = (req, res) => {}
